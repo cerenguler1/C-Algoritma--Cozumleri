15266b7b2c13e1f2f1dedaecf1b3f7bb8899b1bb
cerenguler1 <cereenguler1@gmail.com>
2025-12-25 21:31:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Bir cümle giriniz: ");
+                string cümle = Convert.ToString(Console.ReadLine());
+
+                //anahtar kelimelerimizi tanımlıyoruz
+                string[] keywords = { "sand", "water", "fish", "sun" };
+
+                //büyük/küçük harf sorunu için girilen cümleyiküçük harfe çeviriyoruz
+                string küçükharf = cümle.ToLower();
+
+                //anahtar kelimelerin toplam sayısını tutmak için;
+                int toplam = 0;
+
+
+                for (int i = 0; i < keywords.Length; i++)
+                {
+                    string keyword = keywords[i];
+                    int count = 0;
+
+
+                    for (int a = 0; a <= küçükharf.Length - keyword.Length; a++)
+                    {
+
+                        if (küçükharf.Substring(a, keyword.Length) == keyword)
+                        {
+                            count++;
+                        }
+                    }
+                    toplam += count;
+                }
+                Console.WriteLine(toplam);
+                Console.ReadKey();
+            }
+        }
+    }
+
+
